02b01ede3c1b956a7aa4b9617e7fffe894ba4168
YukiHirayama <yuki.hira26a1@gmail.com>
2022-08-29 12:07:27 +0900

    Initial Commit

---
+++ Renda_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Renda_test
+//
+//  Created by Yuki Hirayama on 2022/08/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
